96fbed1f6a590a73d079215d302c9f9ae7a377bf
Tapio Haapala <tapio.haapala@f-solutions.fi>
2022-05-09 12:50:36 +0300

    Added default phy parameters for WT32-ETH01

--- ArtnetETH.h
+++ ArtnetETH.h
@@ -12,6 +12,39 @@
 #include "Artnet/util/ArxTypeTraits/ArxTypeTraits.h"
 #include "Artnet/util/ArxContainer/ArxContainer.h"
 #ifdef ESP_PLATFORM
+
+// I²C-address of Ethernet PHY (0 or 1 for LAN8720, 31 for TLK110)
+#ifndef ETH_PHY_ADDR
+  #define ETH_PHY_ADDR        1
+#endif
+
+// Type of the Ethernet PHY (LAN8720 or TLK110)
+//typedef enum { ETH_PHY_LAN8720, ETH_PHY_TLK110, ETH_PHY_RTL8201, ETH_PHY_DP83848, ETH_PHY_DM9051, ETH_PHY_KSZ8081, ETH_PHY_MAX } eth_phy_type_t;
+
+#ifndef ETH_PHY_TYPE
+  #define ETH_PHY_TYPE    ETH_PHY_LAN8720
+#endif
+
+// Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
+#ifndef ETH_PHY_POWER
+  #define ETH_PHY_POWER  16
+#endif
+
+// Pin# of the I²C clock signal for the Ethernet PHY
+#ifndef ETH_PHY_MDC
+  #define ETH_PHY_MDC     23
+#endif
+
+// Pin# of the I²C IO signal for the Ethernet PHY
+#ifndef ETH_PHY_MDIO
+  #define ETH_PHY_MDIO    18
+#endif
+
+#ifndef ETH_CLK_MODE
+  #define ETH_CLK_MODE    ETH_CLOCK_GPIO0_IN  //  ETH_CLOCK_GPIO17_OUT
+#endif
+
+
 #include <ETH.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
