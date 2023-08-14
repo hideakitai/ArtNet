1d15b1453ff2c0dbf8e6d58e1325d2ee9c8b0b7c
Hideaki Tai <hideaki.tai@gmail.com>
2023-08-14 11:34:33 +0900

    feat: support NativeEthernet for Teensy 4.1

---
+++ ArtnetNativeEther.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef ARTNET_NATIVE_ETHER_H
+#define ARTNET_NATIVE_ETHER_H
+
+#define ARTNET_ENABLE_ETHER
+
+#include <Arduino.h>
+#include "Artnet/util/ArxTypeTraits/ArxTypeTraits.h"
+#include "Artnet/util/ArxContainer/ArxContainer.h"
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
+#include "Artnet/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
+#include "Artnet/ArtnetCommon.h"
+
+using Artnet = arx::artnet::Manager<EthernetUDP>;
+using ArtnetSender = arx::artnet::Sender<EthernetUDP>;
+using ArtnetReceiver = arx::artnet::Receiver<EthernetUDP>;
+
+#endif  // ARTNET_NATIVE_ETHER_H
+
