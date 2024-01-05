70f69667e882ef2714d760a1324588b06ecdc99d
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2024-01-05 16:18:33 +0100

    Create ArtnetEtherENC.h
    Allows to use https://github.com/JAndrassy/EthernetENC

---
+++ ArtnetEtherENC.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef ARTNET_ETHER_H
+#define ARTNET_ETHER_H
+
+#define ARTNET_ENABLE_ETHER
+
+#include <Arduino.h>
+#include <ArxTypeTraits.h>
+#include <ArxContainer.h>
+#include <EthernetENC.h>
+#include <EthernetUdp.h>
+#include "Artnet/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
+#include "Artnet/Manager.h"
+
+using Artnet = art_net::Manager<EthernetUDP>;
+using ArtnetSender = art_net::Sender<EthernetUDP>;
+using ArtnetReceiver = art_net::Receiver<EthernetUDP>;
+
+#endif  // ARTNET_ETHER_H
