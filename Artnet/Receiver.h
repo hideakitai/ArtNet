8df4eeda26f37cc0c0e61e7a28b876f7571efe00
thirstyice <thirstyice@users.noreply.github.com>
2024-06-29 19:39:39 -0600

    Allow redefinition of where logging goes

--- Artnet/Receiver.h
+++ Artnet/Receiver.h
@@ -49,8 +49,8 @@ public:
 
         if (size > PACKET_SIZE) {
             if (this->b_verbose) {
-                Serial.print(F("Packet size is unexpectedly too large: "));
-                Serial.println(size);
+                log->print(F("Packet size is unexpectedly too large: "));
+                log->println(size);
             }
             size = PACKET_SIZE;
         }
@@ -58,7 +58,7 @@ public:
 
         if (!checkID()) {
             if (this->b_verbose) {
-                Serial.println(F("Packet ID is not Art-Net"));
+                log->println(F("Packet ID is not Art-Net"));
             }
             return OpCode::ParseFailed;
         }
@@ -121,8 +121,8 @@ public:
             }
             default: {
                 if (this->b_verbose) {
-                    Serial.print(F("Unsupported OpCode: "));
-                    Serial.println(this->getOpCode(), HEX);
+                    log->print(F("Unsupported OpCode: "));
+                    log->println(this->getOpCode(), HEX);
                 }
                 op_code = OpCode::Unsupported;
                 break;
@@ -140,19 +140,19 @@ public:
     {
         if (net > 0x7F) {
             if (this->b_verbose) {
-                Serial.println(F("net should be less than 0x7F"));
+                log->println(F("net should be less than 0x7F"));
             }
             return;
         }
         if (subnet > 0xF) {
             if (this->b_verbose) {
-                Serial.println(F("subnet should be less than 0xF"));
+                log->println(F("subnet should be less than 0xF"));
             }
             return;
         }
         if (universe > 0xF) {
             if (this->b_verbose) {
-                Serial.println(F("universe should be less than 0xF"));
+                log->println(F("universe should be less than 0xF"));
             }
             return;
         }
@@ -253,11 +253,11 @@ public:
                 n = num;
             } else {
                 n = size / 3;
-                Serial.println(F("WARN: ArtNet packet size is less than requested LED numbers to forward"));
-                Serial.print(F("      requested: "));
-                Serial.print(num * 3);
-                Serial.print(F("      received : "));
-                Serial.println(size);
+                log->println(F("WARN: ArtNet packet size is less than requested LED numbers to forward"));
+                log->print(F("      requested: "));
+                log->print(num * 3);
+                log->print(F("      received : "));
+                log->println(size);
             }
             for (size_t pixel = 0; pixel < n; ++pixel) {
                 size_t idx = pixel * 3;
@@ -293,11 +293,21 @@ public:
         this->b_verbose = b;
     }
 
+    void logOutputTo(Print* dest) {
+        log = dest;
+    }
+
 protected:
     void attach(S& s)
     {
         this->stream = &s;
     }
+
+#if defined(NO_GLOBAL_INSTANCES) || defined(NO_GLOBAL_SERIAL)
+    Print* log = nullptr; // No Serial -> no output
+#else
+    Print* log = &Serial; // Use Serial by default
+#endif
 
 private:
     bool checkID() const
